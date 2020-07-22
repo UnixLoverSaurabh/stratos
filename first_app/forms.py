e383aa446a1aa996ef12a83b0971143a8b7e63ac
UnixLoverSaurabh <saurabh4104@gmail.com>
2020-07-22 17:43:03 +0530

    validators from django.core

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,14 +1,12 @@
 from django import forms
+from django.core import validators
+
+def check_for_z(value):
+        if value[0].lower() != 'z':
+                raise forms.ValidationError("Name needs to start with Z")
 
 class FormName(forms.Form):
-        name = forms.CharField()
+        name = forms.CharField(validators=[check_for_z])
         email = forms.EmailField()
         text = forms.CharField(widget=forms.Textarea)
-        botcatcher = forms.CharField(required=True, widget=forms.HiddenInput)
-
-        # Custom validation
-        def clean_botcatcher(self):
-                botcatcher = self.cleaned_data['botcatcher']
-                if len(botcatcher) > 0:
-                        raise forms.ValidationError("GOTCHA BOT! ")
-                return botcatcher
+        botcatcher = forms.CharField(required=True, widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
