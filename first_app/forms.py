8c4fa1d7f208a9c3eb63f686eef6f438ce01e95d
UnixLoverSaurabh <saurabh4104@gmail.com>
2020-07-22 19:42:01 +0530

    validator removed to make it simple"

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,12 +1,6 @@
 from django import forms
-from django.core import validators
-
-def check_for_z(value):
-        if value[0].lower() != 'z':
-                raise forms.ValidationError("Name needs to start with Z")
 
 class FormName(forms.Form):
         name = forms.CharField(validators=[check_for_z])
         email = forms.EmailField()
         text = forms.CharField(widget=forms.Textarea)
-        botcatcher = forms.CharField(required=True, widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
